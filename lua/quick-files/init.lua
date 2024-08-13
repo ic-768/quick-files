e1a6d86c774c54d278cf41b70da9af8957047f03
ic768 <ic768@protonmail.com>
2024-08-13 17:06:44 +0300

    Fix error

--- lua/quick-files/init.lua
+++ lua/quick-files/init.lua
@@ -1,6 +1,3 @@
-local window = require("window")
-local navigation = require("navigation")
-
 local M = {}
 M.files = {}
 
@@ -8,9 +5,11 @@ M.get_config_file = function()
 	return vim.fn.stdpath("data") .. "/quick-files.json"
 end
 
-M.save_state = function(state)
+M.save_state = function()
+	local state = { test = 1 }
 	local file_path = M.get_config_file()
 	local file = io.open(file_path, "w+")
+	print("file", file)
 	if file then
 		file:write(vim.fn.json_encode(state))
 		file:close()
@@ -22,9 +21,12 @@ end
 M.setup = function(opts)
 	M.files = opts.files
 
+	local window = require("window")
+
 	vim.keymap.set("n", opts.toggle_map or "<leader>q", window.toggle_window, { noremap = true, silent = true })
-	vim.keymap.set("n", "<CR>", navigation.navigate_to_file, { buffer = window.my_buf, noremap = true, silent = true })
+	--vim.keymap.set("n", "<CR>", navigation.navigate_to_file, { buffer = window.my_buf, noremap = true, silent = true })
 	vim.keymap.set("n", "<Esc>", window.close_window, { buffer = window.my_buf, noremap = true, silent = true })
+	vim.keymap.set("n", "<CR>", M.save_state, { buffer = window.my_buf, noremap = true, silent = true })
 end
 
 return M
--- lua/quick-files/window.lua
+++ lua/quick-files/window.lua
@@ -1,7 +1,8 @@
+local init = require("init")
 local M = {}
 
-local my_buf = vim.api.nvim_create_buf(false, true)
-local window_id = nil
+M.my_buf = vim.api.nvim_create_buf(false, true)
+M.window_id = nil
 
 M.open_floating_window = function()
 	local width = math.floor(vim.o.columns * 0.8)
@@ -17,22 +18,22 @@ M.open_floating_window = function()
 	}
 
 	local keys = {}
-	for k, _ in pairs(files) do
+	for k, _ in pairs(init.files) do
 		table.insert(keys, k)
 	end
 
-	vim.api.nvim_buf_set_lines(my_buf, 0, -1, true, keys)
+	vim.api.nvim_buf_set_lines(M.my_buf, 0, -1, true, keys)
 
-	window_id = vim.api.nvim_open_win(my_buf, true, opts)
+	M.window_id = vim.api.nvim_open_win(M.my_buf, true, opts)
 end
 
 M.close_window = function()
-	vim.api.nvim_win_close(window_id, false)
-	window_id = nil
+	vim.api.nvim_win_close(M.window_id, false)
+	M.window_id = nil
 end
 
 M.toggle_window = function()
-	if window_id then
+	if M.window_id then
 		M.close_window()
 	else
 		M.open_floating_window()
