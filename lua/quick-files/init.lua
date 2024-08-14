78a07e86105f735c977af89d3728220d7285abf6
ic768 <ic768@protonmail.com>
2024-08-14 12:48:08 +0300

    Can add file

--- lua/quick-files/init.lua
+++ lua/quick-files/init.lua
@@ -8,7 +8,7 @@ M.load_state = function()
 	if file then
 		local content = file:read("*a")
 		file:close()
-		local state, err = vim.fn.json_decode(content)
+		local state, err = vim.fn.json_decode(content or {})
 		if state then
 			return state
 		else
@@ -17,6 +17,7 @@ M.load_state = function()
 	else
 		file = io.open(M.config_file, "w")
 		if file then
+			file:write(vim.fn.json_encode({}))
 			file:close()
 			return {} -- Return an empty table if the file is newly created
 		else
@@ -25,8 +26,24 @@ M.load_state = function()
 	end
 end
 
-M.save_state = function()
-	local state = { test = 1 }
+M.add_entry = function()
+	-- Prompt the user for a file label
+	local label = vim.fn.input("Enter file label: ")
+	if label == "" then
+		print("Label cannot be empty")
+		return
+	end
+
+	-- Get the current file path (adjust as needed if this is not the desired file path)
+	local currentFilePath = vim.api.nvim_buf_get_name(0)
+
+	-- Load the existing state or initialize it
+	local state = M.load_state()
+
+	-- Add the new entry
+	state[label] = currentFilePath
+
+	-- Save the updated state
 	local file = io.open(M.config_file, "w+")
 	if file then
 		file:write(vim.fn.json_encode(state))
@@ -42,7 +59,7 @@ M.setup = function(opts)
 	vim.keymap.set("n", opts.toggle_map or "<leader>q", window.toggle_window, { noremap = true, silent = true })
 	--vim.keymap.set("n", "<CR>", navigation.navigate_to_file, { buffer = window.my_buf, noremap = true, silent = true })
 	vim.keymap.set("n", "<Esc>", window.close_window, { buffer = window.my_buf, noremap = true, silent = true })
-	vim.keymap.set("n", "<CR>", M.save_state, { buffer = window.my_buf, noremap = true, silent = true })
+	vim.keymap.set("n", "<leader>G", M.add_entry, { noremap = true, silent = true })
 end
 
 return M
